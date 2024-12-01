1f985568e313509dcf6aa79bc7b0a25ee18daacd
Leo Melo <pgp@leomeloxp.dev>
2024-12-01 13:31:50 +0000

    test: add test for day 01

--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -5,17 +5,17 @@ defmodule AdventOfCode.Day01Test do
 
   @tag :skip
   test "part1" do
-    input = nil
+    {:ok, input} = File.read("priv/input/day_01_example.txt")
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    {:ok, input} = File.read("priv/input/day_01_example.txt")
     result = part2(input)
 
-    assert result
+    assert result == 31
   end
 end
