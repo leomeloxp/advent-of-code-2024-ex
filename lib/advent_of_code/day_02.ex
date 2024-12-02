50cd079af78b2cd2566e0fb9ceca0afec7d0b563
Leo Melo <pgp@leomeloxp.dev>
2024-12-02 23:16:22 +0000

    feat: add logic for day 2 part 1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,68 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  import AdventOfCode.Utils
+  import Utils.GuardPipe
+
+  def part1(args) do
+    input = args || AdventOfCode.Input.get!(2)
+    split_reports(input)
+    |> Enum.filter(fn report ->
+      try do
+        Enum.chunk_every(report, 2, 1, :discard)
+        |> IO.inspect(label: "chunked")
+        |> Enum.reduce(%{direction: nil}, fn [item, next_item], acc ->
+          direction = {item, next_item, acc.direction}
+            |> has_safe_distance?
+            ~> has_same_direction?
+            |>should_halt?
+
+          %{
+            direction: direction
+          }
+        end)
+      catch
+        {:halt, result} -> result
+      end
+    end)
+    |> Enum.count()
   end
 
   def part2(_args) do
   end
+
+  defp split_reports(input) do
+    split_input_by_newline(input)
+    |> Enum.map(fn line ->
+      String.split(line, " ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+  end
+
+  defp has_safe_distance?({item, next_item, direction}) do
+    distance = next_item - item
+    if abs(distance) > 3 || abs(distance) == 0 do
+      {:error, :unsafe_distance}
+    else
+      {:ok, {item, next_item, direction}}
+    end
+  end
+
+  defp has_same_direction?({item, next_item, direction}) do
+    new_direction = (item < next_item && :increasing) || :decreasing
+    if !is_nil(direction) && direction != new_direction do
+      {:error, :wrong_direction}
+    else
+      {:ok, {item, next_item, new_direction}}
+    end
+  end
+
+
+  defp should_halt?({:error, reason}) do
+    IO.inspect(reason, label: "reason")
+    throw({:halt, false})
+  end
+
+  defp should_halt?({:ok, result}) do
+    elem(result, 2)
+  end
+
 end
